200534f3c404b87343b43c8ac7291e6d14019264
Lavish Jain <lavishjain36@gmail.com>
2021-11-07 13:48:52 +0530

    add

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
-const ul = document.getElementById("runners"),
-  url = "https://randomuser.me/api/?gender=female&results=10";
-
-const createNode = (element) => {
-  return document.createElement(element);
-};
-const append = (parent, el) => {
-  return parent.appendChild(el);
-};
-
-fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    let runners = data.results;
-    return runners.map((runner) => {
-      let li = createNode("li"),
-        img = createNode("img"),
-        span = createNode("span");
-      img.src = runner.picture.medium;
-      span.innerHTML = `${runner.name.first} ${runner.name.last}`;
-      append(li, img);
-      append(li, span);
-      append(ul, li);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+document.querySelector("button").addEventListener(
+  "click",
+  function () {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => {
+        return response.json();
+      })
+      .then((myContent) => {
+        myImage.src = myContent["message"];
+      });
+  },
+  false
+);
