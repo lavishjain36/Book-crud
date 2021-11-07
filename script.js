bb03a47152afed16e89ec5e76d77176cbe82a940
Lavish Jain <lavishjain36@gmail.com>
2021-11-07 13:41:22 +0530

    add

--- script.js
+++ script.js
@@ -1,59 +1,30 @@
-// 1.Create 3 function using taking the help Promise
-// 1.getUser()
-// 1.user id:userid
-// 2.username:admin
+const ul = document.getElementById("runners"),
+  url = "https://randomuser.me/api/?gender=female&results=10";
 
-// 2.getServices from APi
-// display user.name
+const createNode = (element) => {
+  return document.createElement(element);
+};
+const append = (parent, el) => {
+  return parent.appendChild(el);
+};
 
-// setTimeout(() => {
-
-// }, 5000);
-
-// EMail and VPN
-
-// 3.getCOst
-
-// service charges
-
-// Output using promise chaining
-
-// 1.Get the data of the user from database
-// 2.Get the data of the services from API
-// array=>email,vpn,cloud access
-
-// 3.FInd out the cost of the service
-
-// one =>1000$ =>3000$
-
-function getUser(userId) {
-  return new Promise((resolve, reject) => {
-    console.log("Getting the data from the database ");
-    setTimeout(() => {
-      console.log("get user");
-      resolve({ userId: userId, username: "admin" });
-    }, 1000);
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    let runners = data.results;
+    return runners.map((runner) => {
+      let li = createNode("li"),
+        img = createNode("img"),
+        span = createNode("span");
+      img.src = runner.picture.medium;
+      span.innerHTML = `${runner.name.first} ${runner.name.last}`;
+      append(li, img);
+      append(li, span);
+      append(ul, li);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
   });
-}
-
-function getServices(user) {
-  return new Promise((resolve, reject) => {
-    console.log(`Getting the Services from ${user.username} `);
-    setTimeout(() => {
-      console.log("get services");
-      resolve(["email", "vpn", "cloud access"]);
-    }, 3000);
-  });
-}
-
-function getCosts(services) {
-  return new Promise((resolve, reject) => {
-    console.log(`Calculating the cost of services for ${services}`);
-    setTimeout(() => {
-      console.log("get costs");
-      resolve(services.length * 1000);
-    }, 3000);
-  });
-}
-
-getUser(100).then(getServices).then(getCosts).then(console.log);
