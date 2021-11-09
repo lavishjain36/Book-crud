16e5ba752e9fd269dcdf30dd220a60092b34aa88
Lavish Jain <lavishjain36@gmail.com>
2021-11-09 08:51:31 +0530

    add

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("button").addEventListener("click", function () {
-  fetch("https://dog.ceo/api/breeds/image/random")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      myImage.src = data.message;
-    });
-});
