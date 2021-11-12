adec1df9a92ad01f647810a16fa0fb81225a2d73
Lavish Jain <lavishjain36@gmail.com>
2021-11-13 01:38:19 +0530

    jikan api

--- script.js
+++ script.js
@@ -1,71 +1,42 @@
-async function getUsers() {
-  const myData = await fetch(
-    "https://618eba7c50e24d0017ce140e.mockapi.io/users",
-    { method: "GET" }
-  );
-  const user = await myData.json();
-  // console.log(user);
-  return user;
+function searchbar() {
+  const main = document.createElement("div");
+  main.innerHTML = `
+    <div class=mainbox></div>
+    <form>
+      <label for="searchbar">Enter a name</label>
+      <input type="text" class="searchon" name="searchbar">
+      <button type="button" class="searchinglist" onclick='search()'>Search</button>
+    </form>
+    `;
+  document.querySelector(".box").append(main);
 }
-async function displayUsers() {
-  const users = await getUsers();
-  // console.log(users)
-  const userList = document.querySelector(".user-list");
-  userList.innerHTML = "";
-  users.forEach((user) => {
-    console.log(user.name);
-    // userList.innerText = userList.innerText + user.name;
-    userList.innerHTML += `<div class="user-container">
-   <img src=${user.avatar} class="image">
-   <div>
-   <h2 class="text">${user.name}</h2>
-   <button onclick="deleteUser(${user.id})">DELETE</button>
-   </div>
-   </div>`;
-  });
-}
-async function deleteUser(id) {
-  console.log(id);
-  //  (IMPORTANT PUT A /(SLASH ON THE LINK OF MOCKAPI))
+searchbar();
 
-  const myData = await fetch(
-    "https://618eba7c50e24d0017ce140e.mockapi.io/users/" + id,
-    { method: "DELETE" }
-  );
-  //   (after click the delete button -> user deleting happens ->refresh the list in mockapi and ->dispalyusers,upto this the user is deleted but it copy with the same user list so we empty the html part and disply it)
-  displayUsers();
+function search() {
+  const test = document.querySelector(".searchon");
+  const inputtext = test.value;
+  fetching(inputtext);
+}
+search();
+
+async function fetching(input) {
+  // try{
+  const data = await fetch(`https://api.jikan.moe/v3/search/anime?q=${input}`);
+  const convertingapi = await data.json();
+  console.log(convertingapi);
+  // convertingapi.forEach(boxes => databox(boxes));
+  databox(convertingapi.results);
+  // }
+  // catch(err){
+  //   document.body.append("Sorry for the inconvenience try again later")
+  // }
 }
 
-async function addUser() {
-  console.log("Adding User");
-  const name = document.querySelector(".add-user-name").value;
-  const avatar = document.querySelector(".add-user-avatar").value;
-  console.log(name, avatar);
-  //(create data)
-  //   1.method - POST
-  //   2.provided data in the - body - stringfy
-  //   3.specify - data format in headers - JSON
-  const myData = await fetch(
-    "https://618eba7c50e24d0017ce140e.mockapi.io/users",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        name: name,
-        avatar: avatar,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-  displayUsers();
+function databox(boxes) {
+  for (let i = 0; i <= boxes.length; i++) {
+    let searchresultbox = document.createElement("div");
+    searchresultbox.innerHTML = `
+      <img src=${boxes[i].image_url}>`;
+    document.querySelector(".box").append(searchresultbox);
+  }
 }
-
-displayUsers();
-
-// (CRUD)
-//              METHODS
-// c - create - POST
-// R - read   - GET
-// U - update - PUT-(Its combination of DELETE &POST)
-// D - Delete - DELETE
