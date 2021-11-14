d2d8a36f4f24ea30ea1175c685542fdc5898ad47
Lavish Jain <lavishjain36@gmail.com>
2021-11-14 19:36:36 +0530

    first commit

--- script.js
+++ script.js
@@ -1,112 +1,133 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyForm = document.querySelector(".container");
-  const toyCollection = document.getElementById("toy-collection");
-  let addToy = false;
+document.addEventListener("DOMContentLoaded", function () {
+  const bookContainer = document.querySelector("#book-container");
+  const bookURL = `http://localhost:3000/books`;
+  const bookForm = document.querySelector("#book-form");
+  let allBooks = [];
 
-  const API_URL = "http://localhost:3000/toys";
-
-  function renderToy(toy) {
-    //create a new Div and attach it to the DOM
-    let toyDiv = document.createElement("div");
-    toyDiv.className = "card";
-    toyDiv.innerHTML = `
-    <h2>${toy.name}</h2>
-    <img src='${toy.image}' class="toy-avatar" />
-    <p>${toy.likes} Likes </p>
-    <button data-id='${toy.id}' class="like-btn">Like <3</button>
-    <button data-id='${toy.id}' class="delete-btn">Delete X</button>
-    `;
-    toyCollection.appendChild(toyDiv);
-  }
-
-  function renderAll(toys) {
-    toyCollection.innerHTML = "";
-    // For each toy render it to the DOM
-    toys.forEach((toy) => {
-      renderToy(toy);
-    });
-  }
-
-  fetch(API_URL)
-    .then(function (resp) {
-      // resp in this case is the response from the server
-      // This comes in the form of a promise object,
-      // we must parse this into usable data
-      return resp.json();
-    })
-    .then((toys) => {
-      // toys is an array that is taken from the promise object that came in the response
-      renderAll(toys);
-    });
-
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyForm.style.display = "block";
-      // submit listener here
-    } else {
-      toyForm.style.display = "none";
-    }
-  });
-
-  toyForm.addEventListener("click", (e) => {
-    let toyName = document.getElementsByClassName("input-text")[0].value;
-    let toyUrl = document.getElementsByClassName("input-text")[1].value;
-    // data stores the data that is passed in by the user as an object
-    data = { name: toyName, image: toyUrl, likes: 0 };
-
-    e.preventDefault();
-    if (e.target.name === "submit") {
-      fetch(`${API_URL}`, {
-        // change the default method from GET to POST
-        method: "POST",
-        // Tells the server we are sending in json
-        headers: {
-          "Content-Type": "application/json",
-        },
-        // sends the data from an object to a json
-        body: JSON.stringify(data),
+  fetch(`${bookURL}`)
+    .then((response) => response.json())
+    .then((bookData) =>
+      bookData.forEach(function (book) {
+        allBooks = bookData;
+        bookContainer.innerHTML += `
+          <div id=book-${book.id}>
+            <h2>${book.title}</h2>
+            <h4>Author: ${book.author}</h4>
+            <img src="${book.coverImage}" width="333" height="500">
+            <p>${book.description}</p>
+            <button data-id=${book.id} id="edit-${book.id}" data-action="edit">Edit</button>
+            <button data-id=${book.id} id="delete-${book.id}" data-action="delete">Delete</button>
+          </div>
+          <div id=edit-book-${book.id}>
+          </div>`;
       })
-        .then((resp) => resp.json())
-        .then((toy) => renderToy(toy));
-      //then we render the response from the API
-    }
-  });
+    ); // end of book fetch
 
-  toyCollection.addEventListener("click", (e) => {
-    //checks to see if the button is a like
-    if (e.target.className === "like-btn") {
-      // like stores the number from the innertext of the like amount
-      let id = parseInt(e.target.dataset.id);
-      let like = parseInt(e.target.previousElementSibling.innerText);
-      like++;
+  bookForm.addEventListener("submit", (e) => {
+    event.preventDefault();
 
-      let data = { likes: like };
+    const titleInput = bookForm.querySelector("#title").value;
+    const authorInput = bookForm.querySelector("#author").value;
+    const coverImageInput = bookForm.querySelector("#coverImage").value;
+    const descInput = bookForm.querySelector("#description").value;
 
-      e.target.previousElementSibling.innerText = `${like} likes`;
-
-      fetch(`${API_URL}/${id}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
+    fetch(`${bookURL}`, {
+      method: "POST",
+      body: JSON.stringify({
+        title: titleInput,
+        author: authorInput,
+        coverImage: coverImageInput,
+        description: descInput,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((book) => {
+        allBooks.push(book);
+        bookContainer.innerHTML += `
+            <div id=book-${book.id}>
+              <h2>${book.title}</h2>
+              <h4>Author: ${book.author}</h4>
+              <img src="${book.coverImage}" width="333" height="500">
+              <p>${book.description}</p>
+              <button data-id=${book.id} id="edit-${book.id}" data-action="edit">Edit</button>
+              <button data-id=${book.id} id="delete-${book.id}" data-action="delete">Delete</button>
+            </div>
+            <div id=edit-book-${book.id}>
+            </div>`;
       });
-    } else if (e.target.className === "delete-btn") {
-      let id = parseInt(e.target.dataset.id);
-      //parent holds the parent node of the button
-      let parent = event.target.parentNode;
-      // this removes the parent which is the current card.
-      parent.remove();
+  }); // end of eventListener for adding a book
 
-      fetch(`${API_URL}/${id}`, {
+  bookContainer.addEventListener("click", (e) => {
+    if (e.target.dataset.action === "edit") {
+      const editButton = document.querySelector(`#edit-${e.target.dataset.id}`);
+      editButton.disabled = true;
+
+      const bookData = allBooks.find((book) => {
+        return book.id == e.target.dataset.id;
+      });
+
+      const editForm = bookContainer.querySelector(
+        `#edit-book-${e.target.dataset.id}`
+      );
+      editForm.innerHTML = `
+            <form class='form' id='edit-book' action='index.html' method='post'>
+              <form id="book-form">
+                <input required id="edit-title" placeholder="${bookData.title}">
+                <input required id="edit-author" placeholder="${bookData.author}">
+                <input required id="edit-coverImage" placeholder="${bookData.coverImage}">
+                <input required id="edit-description" placeholder="${bookData.description}">
+                <input type="submit" value="Edit Book">
+            </form>`;
+
+      editForm.addEventListener("submit", (e) => {
+        event.preventDefault();
+
+        const titleInput = document.querySelector("#edit-title").value;
+        const authorInput = document.querySelector("#edit-author").value;
+        const coverImageInput =
+          document.querySelector("#edit-coverImage").value;
+        const descInput = document.querySelector("#edit-description").value;
+        const editedBook = document.querySelector(`#book-${bookData.id}`);
+
+        fetch(`${bookURL}/${bookData.id}`, {
+          method: "PATCH",
+          body: JSON.stringify({
+            title: titleInput,
+            author: authorInput,
+            coverImage: coverImageInput,
+            description: descInput,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then((book) => {
+            editedBook.innerHTML = `
+                <div id=book-${book.id}>
+                  <h2>${book.title}</h2>
+                  <h4>Author: ${book.author}</h4>
+                  <img src="${book.coverImage}" width="333" height="500">
+                  <p>${book.description}</p>
+                  <button data-id=${book.id} id="edit-${book.id}" data-action="edit">Edit</button>
+                  <button data-id=${book.id} id="delete-${book.id}" data-action="delete">Delete</button>
+                </div>
+                <div id=edit-book-${book.id}>
+                </div>`;
+            editForm.innerHTML = "";
+          });
+      }); // end of this event Listener for edit submit
+    } else if (e.target.dataset.action === "delete") {
+      document.querySelector(`#book-${e.target.dataset.id}`).remove();
+      fetch(`${bookURL}/${e.target.dataset.id}`, {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
         },
-      });
+      }).then((response) => response.json());
     }
-  });
+  }); // end of eventListener for editing and deleting a book
 });
